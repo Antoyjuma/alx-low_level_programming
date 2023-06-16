3418a2f594d6ac9090b68ef28f31c89a73b76ca4
Antonyjuma <antoymalobajuma@gmail.com>
2023-06-16 15:27:33 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program taht prints a line to the standard error
- * Return 1 (Sucess)
+ * Return 1 (Success)
  */
 int main(void)
 {
-	fput("and that place of art is usefull\" - Dora Korpa, 2015-10-19\n" stdout);
+	fputs("and that place of art is usefull\" - Dora Korpa, 2015-10-19\n", stdout);
 	return (1);
 }
