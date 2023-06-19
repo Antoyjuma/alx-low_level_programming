5568a5e7dae846f4bfd49488a3a9f3ad57285227
Antonyjuma <antoymalobajuma@gmail.com>
2023-06-19 05:33:02 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main -  prints all possible different combinations of two digits
+ * Return: Alwyas 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a < 57; a++)
+	for (b = 49; b < 58; b++)
+	{
+		if (b > a)
+		{
+			putchar(a);
+			putchar(b);
+			if (a == 56 && b == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	return (0);
+}
