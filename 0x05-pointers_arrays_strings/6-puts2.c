8b56d74cb6a7d4e0043ff451108ef8c730eef557
Antoyjuma <antoymalobajuma@gmail.com>
2023-07-03 00:07:32 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*puts2 - prints every other character of a string,
+*starting with the first character,
+*@str: string pointer to be printed
+*Return: void
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+		i++;
+	}
+	putchar('\n');
+}
