9089195005fbc55ce256074b1ef109f4a296f709
Antoyjuma <antoymalobajuma@gmail.com>
2023-07-03 09:33:10 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,28 @@
 #include "main.h"
-
 /**
-    *_strspn - function that gets the length of a prefic substring
-    *@s: substring character
-    *@accept: consist of bytes in the inital segment
-    *Return: success
-    */
-
+ * _strspn - Entry point
+ * @s: Input
+ * @accept: Input
+ * Return: Always 0 (Succss)
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (strspn(s, accept));
+	unsigned int n = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
 }
