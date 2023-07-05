7c8da63a4aec600151e72502348966bbd8f691b6
Antoyjuma <antoymalobajuma@gmail.com>
2023-07-05 14:26:04 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: me
- * Desc: Header file containing prototypes for all functions
- * 	used in the 0x08-recursion directory.
- */
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -19,4 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#end
+#endif
