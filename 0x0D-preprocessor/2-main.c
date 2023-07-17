7151571acc4fa2e4cc79dc635a8eda6c9e932e14
Antoyjuma <antoymalobajuma@gmail.com>
2023-07-17 14:17:25 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a function that outputs the file name to standard output.
+ *
+ * Return: 0 will be returned.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
